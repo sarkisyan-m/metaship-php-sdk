032ea5e61f0516557c1f2e3e86670db8f0c59c9f
Михаил Саркисян <sarkisyan@intaro.email>
2020-03-11 14:12:48 +0300

    Обновление класса /DTO/Address/Address.php

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 > Минимальные требования — PHP 7.1+.
 ```bash
-composer require sarkisyan-m/metaship-php-sdk:1.0.0
+composer require sarkisyan-m/metaship-php-sdk:2.0.1
 ```
 
 ### Использование
--- src/DTO/Address/Address.php
+++ src/DTO/Address/Address.php
@@ -8,19 +8,51 @@ class Address
 {
     /**
      * @Serializer\Type("string")
-     * @var string
+     * @var string|null
      */
     public $city;
 
     /**
      * @Serializer\Type("string")
-     * @var string
+     * @var string|null
      */
     public $street;
 
     /**
      * @Serializer\Type("string")
-     * @var string
+     * @var string|null
      */
     public $house;
+
+    /**
+     * @Serializer\Type("string")
+     * @var string|null
+     */
+    public $housing;
+
+    /**
+     * @Serializer\Type("string")
+     * @var string|null
+     */
+    public $region;
+
+    /**
+     * @Serializer\Type("integer")
+     * @var int|null
+     */
+    public $zip;
+
+    /**
+     * @Serializer\SerializedName("kladrId")
+     * @Serializer\Type("string")
+     * @var string|null
+     */
+    public $kladrId;
+
+    /**
+     * @Serializer\SerializedName("fullAddress")
+     * @Serializer\Type("string")
+     * @var string|null
+     */
+    public $fullAddress;
 }
